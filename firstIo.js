506b1ac04d138b8d30943a551dc1d9fe8b038d91
ugrasenanv <ugrasenanv@gmail.com>
2024-02-07 20:35:26 +0530

    second

---
+++ firstIo.js
@@ -0,0 +1,15 @@
+var fileName = process.argv[2];
+var fs = require('fs');
+var buf = null;
+var nbLines = null;
+try {
+	buf = fs.readFileSync('hello.txt', 'utf8');
+	var buffStr = buf.toString();
+	var linesArr = buffStr.split('\n');
+	nbLines = linesArr.length -1; // -1 because of line in file's end
+	console.log(nbLines);
+}
+catch (e) {
+	console.log(e);
+}
+
